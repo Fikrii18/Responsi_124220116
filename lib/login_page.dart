b72e029f37d8245d6e47fa1c01df7b5ef9382236
Fikrii18 <fikrikdi445@gmail.com>
2024-12-05 10:09:29 +0700

    first commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,109 +1,109 @@
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// class LoginPage extends StatefulWidget {
-//   @override
-//   _LoginPageState createState() => _LoginPageState();
-// }
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-// class _LoginPageState extends State<LoginPage> {
-//   final TextEditingController _usernameController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
-//   bool _isPasswordVisible = false;
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
 
-//   void _login() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     String? savedPassword = prefs.getString(_usernameController.text);
+  void _login() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? savedPassword = prefs.getString(_usernameController.text);
 
-//     if (savedPassword != null && savedPassword == _passwordController.text) {
-//       Navigator.pushReplacementNamed(context, '/home', arguments: _usernameController.text);
-//     } else {
-//       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Username atau Password salah')));
-//     }
-//   }
+    if (savedPassword != null && savedPassword == _passwordController.text) {
+      Navigator.pushReplacementNamed(context, '/home', arguments: _usernameController.text);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Username atau Password salah')));
+    }
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         decoration: BoxDecoration(
-//           gradient: LinearGradient(
-//             colors: [Colors.blueAccent, Colors.lightBlue],
-//             begin: Alignment.topLeft,
-//             end: Alignment.bottomRight,
-//           ),
-//         ),
-//         child: Center(
-//           child: Padding(
-//             padding: const EdgeInsets.all(16.0),
-//             child: Card(
-//               elevation: 8,
-//               shape: RoundedRectangleBorder(
-//                 borderRadius: BorderRadius.circular(16),
-//               ),
-//               child: Padding(
-//                 padding: const EdgeInsets.all(20.0),
-//                 child: Column(
-//                   mainAxisSize: MainAxisSize.min,
-//                   children: [
-//                     Text(
-//                       'Login',
-//                     ),
-//                     SizedBox(height: 20),
-//                     TextField(
-//                       controller: _usernameController,
-//                       decoration: InputDecoration(
-//                         labelText: 'Username',
-//                         border: OutlineInputBorder(),
-//                         filled: true,
-//                         fillColor: Colors.grey[200],
-//                       ),
-//                     ),
-//                     SizedBox(height: 10),
-//                     TextField(
-//                       controller: _passwordController,
-//                       obscureText: !_isPasswordVisible,
-//                       decoration: InputDecoration(
-//                         labelText: 'Password',
-//                         border: OutlineInputBorder(),
-//                         filled: true,
-//                         fillColor: Colors.grey[200],
-//                         suffixIcon: IconButton(
-//                           icon: Icon(
-//                             _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-//                           ),
-//                           onPressed: () {
-//                             setState(() {
-//                               _isPasswordVisible = !_isPasswordVisible;
-//                             });
-//                           },
-//                         ),
-//                       ),
-//                     ),
-//                     SizedBox(height: 20),
-//                     ElevatedButton(
-//                       onPressed: _login,
-//                       child: Text('Login'),
-//                       style: ElevatedButton.styleFrom(
-//                         padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-//                         shape: RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.circular(30),
-//                         ),
-//                       ),
-//                     ),
-//                     TextButton(
-//                       onPressed: () {
-//                         Navigator.pushNamed(context, '/register');
-//                       },
-//                       child: Text('Belum punya akun? Daftar'),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blueAccent, Colors.lightBlue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Login',
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _usernameController,
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: !_isPasswordVisible,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _login,
+                      child: Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/register');
+                      },
+                      child: Text('Belum punya akun? Daftar'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
